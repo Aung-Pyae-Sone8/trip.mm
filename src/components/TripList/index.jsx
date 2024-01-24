e070ed140bdd5fcb3fb3de40fc64f5b159b7ec83
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2024-01-24 20:20:36 +0630

    filter-by-useEffect-dependency

--- src/components/TripList/index.jsx
+++ src/components/TripList/index.jsx
@@ -3,18 +3,22 @@ import React, { useEffect, useState } from 'react'
 const TripList = () => {
 
     let [trips, setTrips] = useState([]);
+    let [url, setUrl] = useState('http://localhost:3001/trips');
 
     useEffect(() => {
-        fetch('http://localhost:3001/trips')
+        fetch(url)
             .then(res => res.json())
             .then(data => {
                 setTrips(data);
             })
-    }, []);
+    }, [url]);
 
     return (
         <div>
             <h1>Ready to go?</h1>
+
+            <button onClick={()=> setUrl('http://localhost:3001/trips')}>all</button>
+            <button onClick={()=> setUrl('http://localhost:3001/trips?location=Myanmar')}>Trips in Myanmar</button>
 
             <ul>
                 {trips.map(trip => (
